4d45ab83e66e6b442e5b4fcbf40a9378080f6ab9
javifm86 <javifm86@gmail.com>
2023-04-21 13:20:53 +0200

    update frontend example with config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,25 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import * as path from "path";
+
+const swaggerShimFile =
+  "node_modules/@nestjs/swagger/dist/extra/swagger-shim.js";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react({
+      babel: {
+        plugins: [
+          ["@babel/plugin-proposal-decorators", { legacy: true }],
+          ["@babel/plugin-proposal-class-properties", { loose: true }],
+        ],
+      },
+    }),
+  ],
+  resolve: {
+    alias: {
+      "@nestjs/swagger": path.resolve(__dirname, swaggerShimFile),
+    },
+  },
+});
